88edf7749d4f5dcb0113b3e8398cefb65f7d5cda
OnanThairu <thairuonan@gmail.com>
2022-07-03 16:29:42 +0300

    square class

--- index.js
+++ index.js
@@ -12,3 +12,29 @@ class Polygon {
         return sum
     }
 }
+
+class Triangle extends Polygon {
+    get isValid() {
+        const arr = this.array
+        if (arr[0] + arr[1] <= arr[2] || arr[0] + arr[2] <= arr[1] || arr[1] + arr[2] <= arr[0]) {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+class Square extends Polygon {
+    get area() {
+        return this.array[0] * this.array[1]
+    }
+
+    get isValid() {
+        const arr = this.array
+        if (arr[0] === arr[1] === arr[2] === arr[3]) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
