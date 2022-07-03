6f2524fedb6c4cabbdbf3a87386a26a3123add3d
OnanThairu <thairuonan@gmail.com>
2022-07-03 13:52:26 +0300

    polygon class

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+class Polygon {
+    constructor(array) {
+        this.array = array
+    }
+
+    get countSides() {
+        return this.array.length
+    }
+
+    get perimeter() {
+        const sum = this.array.reduce((prev, curr) => prev + curr)
+        return sum
+    }
+}
